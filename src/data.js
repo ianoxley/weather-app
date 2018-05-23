a58df5a284c2ce04d44f0689c91277648b3e7704
Ian Oxley <ijoxley@gmail.com>
2018-05-23 22:12:49 +0100

    Add mock data
    Add some sample data from OpenWeatherMap for use when building out the
    components.

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  "base": "stations",
+  "clouds": {
+    "all": 0
+  },
+  "cod": 200,
+  "coord": {
+    "lat": -27.76,
+    "lon": 29.92
+  },
+  "dt": 1527107825,
+  "id": 971421,
+  "main": {
+    "grnd_level": 867.21,
+    "humidity": 78,
+    "pressure": 867.21,
+    "sea_level": 1033.93,
+    "temp": 6.57,
+    "temp_max": 6.57,
+    "temp_min": 6.57
+  },
+  "name": "Newcastle",
+  "sys": {
+    "country": "ZA",
+    "message": 0.0036,
+    "sunrise": 1527050338,
+    "sunset": 1527088507
+  },
+  "weather": [
+    {
+      "description": "clear sky",
+      "icon": "01n",
+      "id": 800,
+      "main": "Clear"
+    }
+  ],
+  "wind": {
+    "deg": 309.001,
+    "speed": 1.37
+  }
+}
+export default data;
